b373d21581b630f67d1cb0a924e7fd962ba7ec73
root <root@PC-JRD>
2023-09-26 17:18:59 +0200

    Ajout Job06

--- Job06/my_calculator.sh
+++ Job06/my_calculator.sh
@@ -2,13 +2,13 @@ nombre1="$1"
 symbole="$2"
 nombre2="$3"
 
-if [ "$2" = + ]; 
+if [ "$2" = + ] 
 then resultat=$(("$1" + "$3"))
-elif [ "$2" = - ]; 
+elif [ "$2" = - ] 
 then resultat=$(("$1" - "$3"))
-elif [ "$2" = * ]; 
+elif [ "$2" = * ] 
 then resultat=$(("$1" * "$3"))
-elif [ "$2" = / ]; 
+elif [ "$2" = / ] 
 then resultat=$(("$1" / "$3"))
 else echo "fail"
 fi
