66d3f1cda0419e6804dc67e05a79a030fcbce058
root <root@PC-JRD>
2023-09-26 14:28:00 +0200

    Modification Job05

--- Job05/hello_bye.sh
+++ Job05/hello_bye.sh
@@ -1,6 +1,7 @@
-argument="$1"
-if [ "$argument" = "Hello" ]; then
-    echo "Bonjour, je suis un script"
-elif [ "$argument" = "Bye" ]; then
-    echo "Au revoir et bonne journée"
+if [ "$1" = "Hello" ]
+then
+echo "Bonjour, je suis un script !" 
+elif [ "$1" = "Bye" ]
+then
+echo "Au revoir et bonne journée"
 fi
