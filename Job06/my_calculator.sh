67f72b6ef8d126bfad8a06e3622571665876c80a
jordan-reinaldo <145969884+jordan-reinaldo@users.noreply.github.com>
2023-10-16 10:21:20 +0200

    Update my_calculator.sh

--- Job06/my_calculator.sh
+++ Job06/my_calculator.sh
@@ -2,15 +2,20 @@ nombre1="$1"
 symbole="$2"
 nombre2="$3"
 
-if [ "$2" = + ] 
-then resultat=$(("$1" + "$3"))
-elif [ "$2" = - ] 
-then resultat=$(("$1" - "$3"))
-elif [ "$2" = * ] 
-then resultat=$(("$1" * "$3"))
-elif [ "$2" = / ] 
-then resultat=$(("$1" / "$3"))
-else echo "fail"
+if [ "$2" = + ]
+then
+  resultat=$((nombre1 + nombre2))
+elif [ "$2" = - ]
+then
+  resultat=$((nombre1 - nombre2))
+elif [ "$2" = \* ]
+then
+  resultat=$((nombre1 * nombre2))
+elif [ "$2" = / ]
+then
+  resultat=$((nombre1 / nombre2))
+else
+  echo "fail"
 fi
 
 echo "résultat: $resultat"
